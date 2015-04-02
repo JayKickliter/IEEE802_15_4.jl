20392fe25ad01372d4c854aeef13be3cce1a77d8
Jay Kickliter <jay@kickliter.com>
2015-04-02 13:12:38 -0600

    Added examples

---
+++ examples/GNU Radio/ieee802_15_4_900_MHz_ZMQ.jl
@@ -0,0 +1,15 @@
+using IEEE802_15_4
+using ZMQ
+
+sink  = PacketSink( BPSK, 5, diff_enc = true )
+ctx   = Context( 1 )
+zsock = Socket( ctx, PULL )
+
+connect( zsock, "tcp://localhost:5555" )
+
+while true
+    msg = recv( zsock )
+    out = convert( IOStream, msg )
+    bytes = takebuf_array( out )
+    exec( sink, bytes )
+end
